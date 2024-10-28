92e028eb7f5969995f3b6e192007f05754b621db
Charles Samuels <ks@ks.ax>
2024-10-28 16:42:46 -0700

    add a bin_path option to the Builder
    This parameter sets where initdb et al are searched for, instead of
    $PATH, which makes it possible for test harnesses to just specify
    the known location of the binaries, instead of making them be
    in $PATH, which is not conventional on, at least, Debian
    
    test that if you specify a bad bin_path, it cannot start up database

--- tests/startup.rs
+++ tests/startup.rs
@@ -60,3 +60,23 @@ fn test_tempdb_bringup_shutdown_persist() {
 
     assert!(conf_file.exists());
 }
+
+#[test]
+#[should_panic(expected = "this is not initdb")]
+/// Start a database by specifying the bin_path
+fn test_tempdb_bin_path() {
+    use std::io::Write;
+    use std::os::unix::fs::PermissionsExt;
+
+    let bindir = tempfile::tempdir().unwrap();
+    for name in ["initdb", "createdb", "postgres"] {
+        let path = bindir.path().join(name);
+        let mut f = std::fs::File::create(&path).unwrap();
+        f.write_all(format!("#!/bin/sh\necho this is not {name}\nexit 1").as_bytes())
+            .unwrap();
+        let mut p = f.metadata().unwrap().permissions();
+        p.set_mode(0o700);
+        f.set_permissions(p).unwrap();
+    }
+    let _db = PgTempDB::builder().with_bin_path(&bindir).start();
+}
